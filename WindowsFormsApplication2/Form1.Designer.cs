6eb20102342c5cc3caf70076926010ea447924a1
NinjaBoo <NinjaBoo@DESKTOP-6ADU6Q3>
2016-09-23 17:35:13 -0700

    Add project files.

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMonthlyIncome = new System.Windows.Forms.TextBox();
+            this.txtMonthlyHousingExpense = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Monthly Income:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Monthly Housing Expense:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtMonthlyIncome
+            // 
+            this.txtMonthlyIncome.Location = new System.Drawing.Point(169, 20);
+            this.txtMonthlyIncome.Name = "txtMonthlyIncome";
+            this.txtMonthlyIncome.Size = new System.Drawing.Size(100, 20);
+            this.txtMonthlyIncome.TabIndex = 0;
+            // 
+            // txtMonthlyHousingExpense
+            // 
+            this.txtMonthlyHousingExpense.Location = new System.Drawing.Point(169, 68);
+            this.txtMonthlyHousingExpense.Name = "txtMonthlyHousingExpense";
+            this.txtMonthlyHousingExpense.Size = new System.Drawing.Size(100, 20);
+            this.txtMonthlyHousingExpense.TabIndex = 1;
+            this.txtMonthlyHousingExpense.TextChanged += new System.EventHandler(this.txtMonthlyHousingExpense_TextChanged);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(360, 20);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(72, 52);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(360, 94);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(72, 52);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Answer:";
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(169, 118);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.ReadOnly = true;
+            this.txtAnswer.Size = new System.Drawing.Size(161, 20);
+            this.txtAnswer.TabIndex = 7;
+            this.txtAnswer.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(465, 168);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtMonthlyHousingExpense);
+            this.Controls.Add(this.txtMonthlyIncome);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMonthlyHousingExpense;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.TextBox txtMonthlyIncome;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        string resultOne = ""; // declares a string variable "resultOne"
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double monInc = Convert.ToDouble(txtMonthlyIncome.Text); // takes the input from the user on the form
+                                                                     // "Monthly Income" box and converts it to double
+            double monHouExp = Convert.ToDouble(txtMonthlyHousingExpense.Text); // takes the user input from text box
+                                                                              //"Monthly housing Income" and converts it
+                                                                              // to double
+            double grossInc1 = monInc * .28; // declares a new variable that holds the value of 28% of gross income
+            double grossInc2 = monInc * .20; // declares a new variable that holds the value of 20% of gross income
+
+            if (monHouExp >= grossInc1) // this "if statement" states if variable monHouInc is true
+            {                           // it will assign the string "Not affordable" to variable resultOne
+                resultOne = "Not Affordable"; 
+            }
+            else if (monHouExp >= grossInc2 && monHouExp < grossInc1) // this "else if" statement states if variable 
+            {                                                  // is true it will assign the string "Affordable" to 
+                resultOne = "Affordable";                       // variable resultOne
+            }
+            else if (monHouExp < grossInc1) // this "else if" statement states that if variable monHouExp is less than
+            {                               // variable grossInc1 then it will assing string "Very Affordable" to resultOne
+                resultOne = "Very Affordable";
+            }
+
+            txtAnswer.Text = resultOne.ToString(); // after above code is executed this code converts text box Ansewer
+                                                    // to string type and assings it variable resultOne to display
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close(); // this code states that when Exit button is clicked on form the form will close.
+        }
+
+        private void txtMonthlyHousingExpense_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
