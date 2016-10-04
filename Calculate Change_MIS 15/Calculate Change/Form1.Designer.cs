e90f9dc7919a8001387c6ddcbdeb7ba527df0e35
Omar Enriquez <omarenriquez11@gmail.com>
2016-10-03 22:25:29 -0700

    Add files via upload

---
+++ Calculate Change_MIS 15/Calculate Change/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtAmountOfChangeDue = new System.Windows.Forms.TextBox();
+            this.txtQuarters = new System.Windows.Forms.TextBox();
+            this.txtDimes = new System.Windows.Forms.TextBox();
+            this.txtNickels = new System.Windows.Forms.TextBox();
+            this.txtPennies = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(66, 252);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(182, 252);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Amount of change due(0-99):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(107, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Quarters:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(118, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Dimes:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(112, 160);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Nickels:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(109, 194);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Pennies:";
+            // 
+            // txtAmountOfChangeDue
+            // 
+            this.txtAmountOfChangeDue.Location = new System.Drawing.Point(157, 60);
+            this.txtAmountOfChangeDue.Name = "txtAmountOfChangeDue";
+            this.txtAmountOfChangeDue.Size = new System.Drawing.Size(66, 20);
+            this.txtAmountOfChangeDue.TabIndex = 0;
+            // 
+            // txtQuarters
+            // 
+            this.txtQuarters.Location = new System.Drawing.Point(157, 90);
+            this.txtQuarters.Name = "txtQuarters";
+            this.txtQuarters.ReadOnly = true;
+            this.txtQuarters.Size = new System.Drawing.Size(66, 20);
+            this.txtQuarters.TabIndex = 8;
+            this.txtQuarters.TabStop = false;
+            // 
+            // txtDimes
+            // 
+            this.txtDimes.Location = new System.Drawing.Point(157, 123);
+            this.txtDimes.Name = "txtDimes";
+            this.txtDimes.ReadOnly = true;
+            this.txtDimes.Size = new System.Drawing.Size(66, 20);
+            this.txtDimes.TabIndex = 9;
+            this.txtDimes.TabStop = false;
+            // 
+            // txtNickels
+            // 
+            this.txtNickels.Location = new System.Drawing.Point(157, 157);
+            this.txtNickels.Name = "txtNickels";
+            this.txtNickels.ReadOnly = true;
+            this.txtNickels.Size = new System.Drawing.Size(66, 20);
+            this.txtNickels.TabIndex = 10;
+            this.txtNickels.TabStop = false;
+            // 
+            // txtPennies
+            // 
+            this.txtPennies.Location = new System.Drawing.Point(157, 191);
+            this.txtPennies.Name = "txtPennies";
+            this.txtPennies.ReadOnly = true;
+            this.txtPennies.Size = new System.Drawing.Size(66, 20);
+            this.txtPennies.TabIndex = 11;
+            this.txtPennies.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(344, 316);
+            this.Controls.Add(this.txtPennies);
+            this.Controls.Add(this.txtNickels);
+            this.Controls.Add(this.txtDimes);
+            this.Controls.Add(this.txtQuarters);
+            this.Controls.Add(this.txtAmountOfChangeDue);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "Form1";
+            this.Text = "Calculate Change";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtAmountOfChangeDue;
+        private System.Windows.Forms.TextBox txtQuarters;
+        private System.Windows.Forms.TextBox txtDimes;
+        private System.Windows.Forms.TextBox txtNickels;
+        private System.Windows.Forms.TextBox txtPennies;
+    }
+}
+
---
+++ Calculate Change_MIS 15/Calculate Change/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            int numberOfChange = Convert.ToInt32(txtAmountOfChangeDue.Text);
+            int numberOfQuarters = 0;
+            int numberOfDimes = 0;
+            int numberOfNickels = 0;
+            int numberOfPennies = 0;
+            int changeFromQuarters = 0;
+            int changeFromDimes = 0;
+            int changeFromNickels = 0;
+
+            if (numberOfChange > 0) ;
+            {
+                numberOfQuarters = numberOfChange / 25;
+                changeFromQuarters = numberOfChange % 25;
+            }
+
+            if (changeFromQuarters > 0) ;
+            {
+                numberOfDimes = changeFromQuarters / 10;
+                changeFromDimes = changeFromQuarters % 10;
+            }
+
+            if (changeFromDimes > 0) ;
+            {
+                numberOfNickels = changeFromDimes / 5;
+                changeFromNickels = changeFromDimes % 5;
+            }
+
+            if (changeFromNickels > 0) ;
+            {
+                numberOfPennies = changeFromNickels / 1;
+            }
+
+            txtQuarters.Text = numberOfQuarters.ToString();
+            txtDimes.Text = numberOfDimes.ToString();
+            txtNickels.Text = numberOfNickels.ToString();
+            txtPennies.Text = numberOfPennies.ToString();
+
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close(); 
+        }
+    }
+}
